d1d983aacf9c9a68569a38f3721a2183988e5b06
cyberllama <colin@kirby.net>
2023-12-01 13:12:06 -0500

    modified app variable name

--- app.py
+++ app.py
@@ -7,22 +7,18 @@ from api.route.geojson import geojson_api
 from flask_cors import CORS
 
 def create_app():
-    app = Flask(__name__)
-    CORS(app)
+    application = Flask(__name__)
+    CORS(application)
 
-    app.config['SWAGGER'] = {
-        'title': 'Flask API Starter Kit',
-    }
-    swagger = Swagger(app)
      ## Initialize Config
     # app.config.from_pyfile('config.py')
-    app.register_blueprint(home_api, url_prefix='/')
-    app.register_blueprint(crash_api, url_prefix='/crash')
-    app.register_blueprint(image_api, url_prefix='/image')
-    app.register_blueprint(geojson_api, url_prefix='/geojson')
+    application.register_blueprint(home_api, url_prefix='/')
+    application.register_blueprint(crash_api, url_prefix='/crash')
+    application.register_blueprint(image_api, url_prefix='/image')
+    application.register_blueprint(geojson_api, url_prefix='/geojson')
 
 
-    return app
+    return application
 
 
 if __name__ == '__main__':
@@ -33,9 +29,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
     port = args.port
 
-    app = create_app()
+    application = create_app()
 
-    app.run(host='0.0.0.0', port=port)
+    application.run(host='0.0.0.0', port=port)
 
 
 
